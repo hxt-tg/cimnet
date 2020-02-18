8ebedaac9ee03bb44589bea36805093520e23bfd
hxt-tg <782920705@qq.com>
2020-02-19 00:29:13 +0800

    test: Add copy constructor test.

--- test/test_base.cc
+++ test/test_base.cc
@@ -8,6 +8,25 @@ typedef struct {
 } KindOfData;
 
 
+void print_node_edge_data(Network<int, int, KindOfData> &net,
+        std::string tag) {
+    std::cout << "Node data: (" << tag << ")" << std::endl;
+    for (auto i: net.nodes())
+        std::cout << " Node " << i << "   data=" << net.node(i) << std::endl;
+    std::cout << "Edge data: (" << tag << ")" << std::endl;
+    for (auto e: net.edges()) {
+        try {
+            std::cout << "[" << e.first << "->" << e.second << "] desc="
+                << net.edge(e.first, e.second).desc
+                << "  amount=" << net.edge(e.first, e.second).amount
+                << std::endl;
+        } catch (const NetworkException &e) {
+            std::cout << e.what() << std::endl;
+            continue;
+        }
+    }
+}
+
 void test_construct_net(void) {
     WellMixNet<int, KindOfData> net(3);
     net.add_edge(2, 1, {"123", 3});
@@ -20,24 +39,15 @@ void test_construct_net(void) {
     for (auto n : net.neighbors(1))
         std::cout << n << " ";
     std::cout << std::endl << std::endl;
+    print_node_edge_data(net, "net");
 
-    std::cout << "Node data:" << std::endl;
-    for (auto i: net.nodes())
-        std::cout << " Node " << i << "   data=" << net.node(i) << std::endl;
-
-    std::cout << "Edge data:" << std::endl;
-    for (auto e: net.edges()) {
-        try {
-            std::cout << "[" << e.first << "->" << e.second << "] desc="
-                << net.edge(e.first, e.second).desc
-                << "  amount=" << net.edge(e.first, e.second).amount
-                << std::endl;
-        } catch (const NetworkException &e) {
-            std::cout << e.what() << std::endl;
-            continue;
-        }
-    }
-    std::cout << "End..." << std::endl;
+    Network<int, int, KindOfData> net_copy(net);
+    net_copy.edge(1, 3).desc = "update by copy";
+    net_copy[1] = 999;
+    std::cout << "=================" << std::endl;
+    print_node_edge_data(net, "net origin");
+    std::cout << "=================" << std::endl;
+    print_node_edge_data(net_copy, "net copy");
 }
 
 void test_modify_net() {
@@ -86,8 +96,8 @@ void test_efficiency() {
 
 int main(void) {
     test_construct_net();
-    test_modify_net();
-    test_efficiency();
+    /* test_modify_net(); */
+    /* test_efficiency(); */
     return 0;
 }
 
